147bc7d0d18c04a464f57f57a740a5bc06bf3792
SAJID-AFNAN <sajidafnan91@gmail.com>
2025-07-01 16:50:35 +0600

    hook and function set

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useCallback, useState } from "react"
+
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [number, serNumber] = useState(false)
+  const [character, setCharacter] = useState(false)
+  const [password, setPassword] = useState("")
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if (number) str += "0123456789"
+    if (character) str += "!@#$%^&*(){}[]"
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+
+      pass = str.charAt(char)
+
+    }
+    setPassword(pass)
+  }, [length, number, character, setPassword])
+
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700 text-center">
+        <h1 className="text-white ">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none "
+            placeholder="Password"
+            readOnly />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
